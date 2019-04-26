e7235283b6fad153ef244877f528722eb40343c9
DarWinDa <1579756143@qq.com>
2019-04-26 09:57:37 +0800

    增加注释

--- service-ribbon/src/main/java/com/example/demo/service/HelloService.java
+++ service-ribbon/src/main/java/com/example/demo/service/HelloService.java
@@ -1,14 +1,21 @@
 package com.example.demo.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import javax.annotation.Resource;
+
+/**
+ * @author zhaoda
+ */
 @Service
 public class HelloService {
 
-    @Autowired
-    RestTemplate restTemplate;// 通过之前注入ioc容器的restTemplate来消费service-hi服务的“/client”接口
+    /**
+     * 通过之前注入ioc容器的restTemplate来消费service-hi服务的“/client”接口
+     */
+    @Resource
+    RestTemplate restTemplate;
 
     public String hiService(String name) {
        return restTemplate.getForObject("http://service-hi/hi?name="+name,String.class);
